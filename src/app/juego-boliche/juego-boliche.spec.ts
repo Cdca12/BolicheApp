498df4ebc07d0d6afb2c3ecc25f32f83192358b9
Carlos Contreras <cdca12mx@gmail.com>
2020-11-30 23:32:38 -0700

    Se crea clase JuegoBoliche con Unit Test

---
+++ src/app/juego-boliche/juego-boliche.spec.ts
@@ -0,0 +1,7 @@
+import { JuegoBoliche } from './juego-boliche';
+
+describe('JuegoBoliche', () => {
+  it('should create an instance', () => {
+    expect(new JuegoBoliche()).toBeTruthy();
+  });
+});
